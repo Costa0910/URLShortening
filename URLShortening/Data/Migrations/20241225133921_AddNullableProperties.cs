bb4636d67814a48ed690a93d9707a82ef4c741a1
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2025-01-03 11:52:50 +0000

    Add initial migrations for database setup
    -- Introduce database schema setup for URL shortening, user authentication, and logging.
    -- This includes tables for users, roles, short URLs, and access logs with necessary constraints and indexes.
    -- Also adds nullable property adjustments in subsequent migration.

---
+++ URLShortening/Data/Migrations/20241225133921_AddNullableProperties.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortening.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullableProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Urls_CustomAlias",
+                table: "Urls");
+
+            migrationBuilder.DropColumn(
+                name: "CustomAlias",
+                table: "Urls");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ref",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IPAddress",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomAlias",
+                table: "Urls",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ref",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IPAddress",
+                table: "AccessLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_CustomAlias",
+                table: "Urls",
+                column: "CustomAlias",
+                unique: true,
+                filter: "[CustomAlias] IS NOT NULL");
+        }
+    }
+}
